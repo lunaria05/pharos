95b6bcda760d40770c831838549f512c242a2c2c
Hiralvala <valahiral563@gmail.com>
2025-10-19 12:58:27 +0530

    Added navbar in header

--- src/app/raffle/[id]/page.tsx
+++ src/app/raffle/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Navbar from '@/Components/Navbar/Navbar'
 import IndividualRaffle from '@/Components/Raffle/IndividualRaffle'
 import { getRaffleById } from '@/data/raffle'
 import { notFound } from 'next/navigation'
@@ -18,6 +19,7 @@ const page = ({ params }: PageProps) => {
 
   return (
     <div className=" min-h-screen border-4 border-black shadow-[4px_4px_0px_#1a202c] my-10  max-w-[1200px] 2xl:max-w-[1400px] mx-auto bg-white">
+        <Navbar/>
       <IndividualRaffle raffle={raffle} />
     </div>
   )
